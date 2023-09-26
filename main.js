208525a0f86f5fe12fd2f1d5e801c0e95e0c1053
sangeethathallapelly <146064909+sangeethathallapelly@users.noreply.github.com>
2023-09-26 12:58:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var score=0;
+
+function decrease(){
+    score=score-10;
+    document.getElementById("scorelabel").textContent=score;
+}
+function increase(){
+    score=score+10;
+    document.getElementById("scorelabel").textContent=score;
+}
+    
